27fde1d0044de8ac4f7d745d4df2a502dbe158f5
psyconach <ignacio.jana95@gmail.com>
2025-05-11 09:17:08 -0400

    modulando templates

---
+++ core/backends.py
@@ -0,0 +1,19 @@
+# core/backends.py
+from django.contrib.auth.backends import ModelBackend
+from .models import Usuario
+
+class EmailOrRutBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            # Intenta por email primero
+            user = Usuario.objects.get(email=username)
+        except Usuario.DoesNotExist:
+            try:
+                # Luego por RUT
+                user = Usuario.objects.get(rut=username)
+            except Usuario.DoesNotExist:
+                return None
+        
+        if user.check_password(password):
+            return user
+        return None
